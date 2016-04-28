c5fc2abf66fb88592984e94320da86bfbc4fd411
peiying <277079165@qq.com>
2016-04-28 17:29:14 +0800

    111

--- MINIST_simple.py
+++ MINIST_simple.py
@@ -23,7 +23,6 @@ def backpropagate(network, input_vector, targets):
     hidden_outputs, outputs = feed_forward(network, input_vector)
     # the output * (1 - output) is from the derivative of sigmoid || zip: convert to matrix
     output_deltas = [output * (1 - output) * (output - target) for output, target in zip(outputs, targets)]
-
     # adjust weights for output layer, one neuron at a time
     for i, output_neuron in enumerate(network[-1]):
         # focus on the ith output layer neuron
@@ -42,18 +41,38 @@ def backpropagate(network, input_vector, targets):
 def predict(input):
     return feed_forward(network, input)[-1]
 
+def loadDataSet():
+    dataMat = []
+    fr = open('digit.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        tempList = []
+        for i in range(len(lineArr)):
+            tempList.append(int(lineArr[i]))
+        dataMat.append(tempList)
+    return dataMat
 random.seed(0)
 input_size = 25
 num_hidden = 5
 output_size = 10
 #generate 5*26 matrix to represent hidden_layer's weights
 hidden_layer = [[random.random() for __ in range(input_size + 1)] for __ in range(num_hidden)]
+
 #generate 10*6 matrix to represent output_layer's weights
 output_layer = [[random.random() for __ in range(num_hidden + 1)] for __ in range(output_size)]
 
 network = [hidden_layer, output_layer]
-for __ in range(10000):
+
+targets = [[1 if i == j else 0 for i in range(10)] for j in range(10)]
+inputs = loadDataSet()
+for i in range(10000):
+    if(i % 1000 == 0):
+        print network
     for input_vector, target_vector in zip(inputs, targets):
         backpropagate(network, input_vector, target_vector)
 
-predict(inputs[7])
+result = predict(inputs[8])
+print result
+for i in range(len(result)):
+    if result[i] > 0.5:
+        print 'predict to ' + str(i)
